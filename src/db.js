06ebcaa3f5c8ed8ab9b445cdee730555187e46b4
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2022-07-09 03:29:00 +0100

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,9 @@
+const { DB_USER, DB_PASS } = process.env;
+//console.log({ DB_USER, DB_PASS });
+/*
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.dognspk.mongodb.net/?retryWrites=true&w=majority`
+);
+
+/*//
